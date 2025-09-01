357ea6e496fc6f9d16315ae4d5de6ed25ff5f582
Luciano Dittgen <issotemfuturo@gmail.com>
2025-09-01 10:22:58 -0300

    BackEnd1

---
+++ Faculdade/BackEnd1/html/exemplo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Primeiro Programa PHP </title>
+</head>
+<?php
+    echo "<p> Olá Mundo </p>";
+    echo "<h1>Seja Bem-vindo </h1>";
+?>
+        </body>
+</html> 
